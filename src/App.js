34d607bdc94147e2b5142e19f374673d745b9ae6
rockydey <coderrocky25@gmail.com>
2022-06-10 13:36:51 +0600

    footer section complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home';
+import Footer from './Pages/Shared/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Pages/Shared/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-secondary py-5'>
+            <h1 className='text-center text-white'>All © Rights Reserved to Rocky Dey 2022</h1>
+        </div>
+    );
+};
+
+export default Footer;
